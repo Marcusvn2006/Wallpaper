44ffc93a8bd92b056a47fd6feca3eaef3a6c860f
MARCUS ROMÃO <marcus.romao@portalsesisp.org.br>
2024-02-21 09:06:59 -0300

    terminado

---
+++ javascipt.js
@@ -0,0 +1,18 @@
+const $next = document.querySelector('.next');
+const $prev = document.querySelector('.prev');
+
+$next.addEventListener(
+    'click',
+    ()=>{
+        const item = document.querySelectorAll('.item');
+        document.querySelector('.slide').appendChild(item[0]);
+    },
+);
+$prev.addEventListener(
+    'click',
+    ()=>{
+        const item = document.querySelectorAll('.item');
+        document.querySelector('.slide').prepend(item[item.length-1]);
+
+    },
+);
